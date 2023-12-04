665b63aefcfcd94b6da911ce6c3fb3fb54f38169
logant08 <logantillman@ymail.com>
2023-12-03 22:35:47 -0600

    Add constructor annotations

--- backend/src/main/java/logan/tillman/ecom/dto/CategoryDTO.java
+++ backend/src/main/java/logan/tillman/ecom/dto/CategoryDTO.java
@@ -1,10 +1,14 @@
 package logan.tillman.ecom.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryDTO {
     Integer categoryId;
     String name;
--- backend/src/main/java/logan/tillman/ecom/dto/ProductDTO.java
+++ backend/src/main/java/logan/tillman/ecom/dto/ProductDTO.java
@@ -1,12 +1,16 @@
 package logan.tillman.ecom.dto;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.OffsetDateTime;
 import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductDTO {
     Integer productId;
     String title;
